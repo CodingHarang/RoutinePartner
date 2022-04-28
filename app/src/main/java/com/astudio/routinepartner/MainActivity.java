86f1d7e9c2387829918353824299392ca62f953c
CodingHarang <txepahs@gmail.com>
2022-04-28 16:59:22 +0900

    [YJS] Merge YJS, PSY

--- app/src/main/java/com/astudio/routinepartner/MainActivity.java
+++ app/src/main/java/com/astudio/routinepartner/MainActivity.java
@@ -54,6 +54,14 @@ public class MainActivity extends AppCompatActivity {
     protected ArrayList<ActInfo> ActInfoList;
     protected ArrayList<ActInfoItem> ActInfoItemList = new ArrayList<ActInfoItem>();
 
+
+    LottieAnimationView PetView;
+    TextView PetName;
+    String action="meal"; //ActInfoItem 에서 가져옴
+    Context MainContext;
+    String text_PetName="";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -185,6 +193,81 @@ public class MainActivity extends AppCompatActivity {
         });
         //-------------------------------------------------------------------->LSY
         //-------------------------------------------------------------------->LSY
+
+        //<--------------------------------------------------------------------PSY
+        //<--------------------------------------------------------------------PSY
+
+
+
+        MainContext=this;
+        text_PetName=PreferenceManage.getString(MainContext,"rebuild");
+        if(text_PetName.isEmpty()){
+            text_PetName="";
+        }
+        PetName=(TextView) findViewById(R.id.PetName);
+        PetName.setText(text_PetName);
+
+        PetView=findViewById(R.id.lottie);
+        PetView.setAnimation("HappyDog.json");
+        PetView.setRepeatCount(2);
+        PetView.playAnimation();
+
+        PetView.addAnimatorListener(new Animator.AnimatorListener() {
+            FadeInAndOut FadeAnimation=new FadeInAndOut();
+            @Override
+            public void onAnimationStart(Animator animator) {
+                ImageView img=(ImageView) findViewById(R.id.imageView);
+                switch(action){
+                    case "meal":
+                        img.setImageResource(R.drawable.foodbowl);
+                        FadeAnimation.fadeOutImage(img);
+                        break;
+                    case "sleep":
+                        img.setImageResource(R.drawable.bed);
+                        FadeAnimation.fadeOutImage(img);
+                        break;
+                    case "study":
+                        img.setImageResource(R.drawable.book);
+                        FadeAnimation.fadeOutImage(img);
+                        break;
+                    default:
+                }
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                PetView.setOnClickListener((view -> {
+                    action="";
+                    PetView.setRepeatCount(1);
+                    PetView.playAnimation();
+                }));
+
+                PetView.setOnLongClickListener((view) -> {
+                    action="interaction";
+                    ImageView HeartImage=findViewById(R.id.HeartImage);
+                    HeartImage.setImageResource(R.drawable.heart);
+                    FadeAnimation.fadeInImage(HeartImage);
+                    PetView.setRepeatCount(1);
+                    PetView.playAnimation();
+                    return true;
+                });
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
+
+        //-------------------------------------------------------------------->PSY
+        //-------------------------------------------------------------------->PSY
     }
 
 
@@ -363,4 +446,54 @@ public class MainActivity extends AppCompatActivity {
     }*/
     //-------------------------------------------------------------------->LSY
     //-------------------------------------------------------------------->LSY
+
+
+    //<--------------------------------------------------------------------PSY
+    //<--------------------------------------------------------------------PSY
+
+
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.setup_menu,menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.NameSet:{
+                AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("펫 이름 설정");
+                builder.setMessage("이름을 입력하시오.");
+                final EditText editText=new EditText(MainActivity.this);
+                builder.setView(editText);
+
+                builder.setPositiveButton("입력", new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog,int which){
+                        String name;
+                        name=editText.getText().toString();
+                        PetName.setText(name);
+                        PreferenceManage.setString(MainContext, "rebuild", name);
+                        dialog.dismiss();
+                    }
+                });
+                builder.setNegativeButton("취소",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+
+                builder.show();
+            } break;
+
+            default:
+        }
+        return true;
+    }
+
+
+
+    //-------------------------------------------------------------------->PSY
+    //-------------------------------------------------------------------->PSY
 }
