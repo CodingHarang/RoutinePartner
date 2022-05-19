3bf34a6cde320f55541a61f128691ca405952c3e
CodingHarang <txepahs@gmail.com>
2022-05-19 22:07:36 +0900

    [YJS] Chore

--- app/src/main/java/com/astudio/routinepartner/SavedSettings.java
+++ app/src/main/java/com/astudio/routinepartner/SavedSettings.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class SavedSettings {
-    public static ArrayList<String> CategoryList = new ArrayList<>(Arrays.asList("?‹?‚¬", "?ˆ˜ë©?", "ê³µë?€", "ê²Œì„"));
+    public static ArrayList<String> CategoryList = new ArrayList<>(Arrays.asList("ì‹ì‚¬", "ìˆ˜ë©´", "ê³µë¶€", "ê²Œì„"));
     public static ArrayList<Integer> ColorList = new ArrayList<>(Arrays.asList(0XFF85C8F5, 0xFF077ECE, 0XFFFFFF00, 0XFF00FFFF));
     public static ArrayList<Integer> GoalList = new ArrayList<>(Arrays.asList(3, 7));
 }
