31f2968e9cfbb4bced6151401bdfef6dd69f050e
CodingHarang <txepahs@gmail.com>
2022-05-19 21:57:20 +0900

    [YJS] Add SavedSettings
    - 누락된 SavedSettings.java 추가

---
+++ app/src/main/java/com/astudio/routinepartner/SavedSettings.java
@@ -0,0 +1,10 @@
+package com.astudio.routinepartner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class SavedSettings {
+    public static ArrayList<String> CategoryList = new ArrayList<>(Arrays.asList("식사", "수면", "공부", "게임"));
+    public static ArrayList<Integer> ColorList = new ArrayList<>(Arrays.asList(0XFF85C8F5, 0xFF077ECE, 0XFFFFFF00, 0XFF00FFFF));
+    public static ArrayList<Integer> GoalList = new ArrayList<>(Arrays.asList(3, 7));
+}
