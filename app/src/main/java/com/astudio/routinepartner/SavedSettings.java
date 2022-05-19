17ebe711df9898fdc56cde0f47d574a3d1bf887f
lsy <78686421+smallbigsmall@users.noreply.github.com>
2022-05-19 22:32:29 +0900

    [LSY]

--- app/src/main/java/com/astudio/routinepartner/SavedSettings.java
+++ app/src/main/java/com/astudio/routinepartner/SavedSettings.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class SavedSettings {
-    public static ArrayList<String> CategoryList = new ArrayList<>(Arrays.asList("식사", "수면", "공부", "게임"));
+    public static ArrayList<String> CategoryList = new ArrayList<>(Arrays.asList("식사", "수면", "공부"));
     public static ArrayList<Integer> ColorList = new ArrayList<>(Arrays.asList(0XFF85C8F5, 0xFF077ECE, 0XFFFFFF00, 0XFF00FFFF));
     public static ArrayList<Integer> GoalList = new ArrayList<>(Arrays.asList(3, 7));
+    public static ArrayList<String> StatList = new ArrayList<>(Arrays.asList("지능", "재미", "체력", "포만감", "잔고", "자아실현"));
 }
