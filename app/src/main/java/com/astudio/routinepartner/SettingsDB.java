1f564e3840d80d0d193d6ea3d944895f05611a9a
CodingHarang <txepahs@gmail.com>
2022-05-20 09:50:06 +0900

    [YJS] Add Settings DB
    - 설정 관련 데이터가 저장될 데이터베이스 구축

---
+++ app/src/main/java/com/astudio/routinepartner/SettingsDB.java
@@ -0,0 +1,36 @@
+package com.astudio.routinepartner;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+// Database Object
+@Database(entities = {Settings.class}, version = 1, exportSchema = false)
+public abstract class SettingsDB extends RoomDatabase {
+
+    private static volatile SettingsDB INSTANCE = null;
+    public abstract SettingsDAO settingDao();
+    static final ExecutorService DatabaseWriteExecutor = Executors.newSingleThreadExecutor();
+
+    // Database가 열려있지 않은 경우 연다
+    static SettingsDB getDatabase(final Context context) {
+        if(INSTANCE == null) {
+            synchronized (SettingsDB.class) {
+                if(INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), SettingsDB.class, "settings_database").build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    // Database를 close한다
+    public static void destroyInstance() {
+        INSTANCE = null;
+    }
+}
